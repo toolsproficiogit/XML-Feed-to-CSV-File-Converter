abdd99abe74288dfa3b6b29a006527b4215b898c
toolsproficiogit <tools@proficio.cz>
2025-12-03 11:49:57 +0100

    Delete xml-feed-to-csv-file-converter directory

--- xml-feed-to-csv-file-converter/constants.ts
+++
@@ -1,3 +0,0 @@
-export const CHUNK_SIZE = 1024 * 64; // 64KB chunks for reading
-export const ANALYSIS_ITEM_LIMIT = 500; // Analyze first 500 items to guess schema
-export const CSV_SEPARATOR = ',';
